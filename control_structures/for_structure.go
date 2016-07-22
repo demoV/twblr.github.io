5c758d430b43f7192babad5f9050481d1695fd91
Lalit Pandey <lalitpan@thoughtworks.com>
2016-07-22 16:11:19 +0530

    passed test of for loop

--- control_structures/for_structure.go
+++ control_structures/for_structure.go
@@ -1,5 +1,16 @@
 package control_structures
 
 func collatzChainLength(n int) int {
-	return 0
+	cons := 0
+	for (n != 1) {
+		if(n % 2 != 0) {
+			n = 3 * n + 1
+			cons ++
+		}else{
+			n = n / 2
+			cons ++
+		}	
+	}
+	return cons
+	
 }
