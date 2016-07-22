1bde8e3afd085299796bd2a4348e444d08e545d1
Lalit Pandey <lalitpan@thoughtworks.com>
2016-07-22 17:48:53 +0530

    passed map test

--- data_structures/maps.go
+++ data_structures/maps.go
@@ -1,9 +1,20 @@
 package data_structures
 
 func findPeopleWithCommonInterest(data map[string][]string, interest string) []string {
-	return nil
+	aSlice := make([]string, 0)
+	for key,value := range data{
+		if contains(value, interest) {
+			aSlice = append(aSlice, key)
+		}
+	}
+	return aSlice
 }
 
 func contains(src []string, elem string) bool {
+	for _,value := range src{
+		if(value == elem){
+			return true
+		}
+	}
 	return false
 }
