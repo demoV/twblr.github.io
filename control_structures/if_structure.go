8cda51ab196c5e5408bac6884ec9bdff1a954d23
Lalit Pandey <lalitpan@thoughtworks.com>
2016-07-22 15:27:20 +0530

    initial commit

--- control_structures/if_structure.go
+++ control_structures/if_structure.go
@@ -1,5 +1,13 @@
 package control_structures
 
+import "strconv"
 func fizzBuzz(i int32) string {
+	if i % 3 == 0 && i % 5 == 0{
+		return "FizzBuzz"
+	}else if i % 3 == 0{
+		return "Fizz"
+	}else if i % 5 == 0{
+		return "Buzz"	
+	}
 	return ""
 }
